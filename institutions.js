004177253c9778cde18f60dda5a5531a297e7ab9
Nelson Oniwon <vamioniwon@gmail.com>
2025-11-20 00:28:37 +0100

    Initial JusticeBot backend upload

---
+++ institutions.js
@@ -0,0 +1,52 @@
+module.exports = {
+  police_abuse: [
+    {
+      name: "Inspector-General of Police (IGP)",
+      title: "Inspector-General of Police",
+      email: "npfcomplaints@npf.gov.ng",
+      whatsapp: "08057000001"
+    },
+    {
+      name: "Police Complaint Response Unit (PCRU)",
+      title: "Head, PCRRU",
+      email: "npfcomplaints@npf.gov.ng",
+      whatsapp: "08057000001"
+    },
+    {
+      name: "Police Service Commission (PSC)",
+      title: "Chairman, Police Service Commission",
+      email: "complaints@psc.gov.ng",
+      whatsapp: null
+    },
+    {
+      name: "National Human Rights Commission (NHRC)",
+      title: "Executive Secretary, NHRC",
+      email: "info@nhrc.gov.ng",
+      whatsapp: null
+    }
+  ],
+
+  unpaid_salary: [
+    {
+      name: "Federal Ministry of Labour & Employment",
+      title: "Director, Complaints Desk",
+      email: "labourcomplaints@labour.gov.ng",
+      whatsapp: null
+    },
+    {
+      name: "Industrial Arbitration Panel (IAP)",
+      title: "Chief Registrar, IAP",
+      email: "info@iap.gov.ng",
+      whatsapp: null
+    }
+  ],
+
+  general_complaint: [
+    {
+      name: "Public Complaints Commission (PCC)",
+      title: "Honourable Chief Commissioner",
+      email: "info@pcc.gov.ng",
+      whatsapp: null
+    }
+  ]
+};
